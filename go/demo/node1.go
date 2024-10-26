70811461fcdcb935efd2a72a91ee688b5d30cf3d
Abhishek Raj <abhi@raj.me>
2024-10-26 05:39:49 +0530

    Add badger datastore (go-ds-badger3)

--- go/demo/node1.go
+++ go/demo/node1.go
@@ -13,6 +13,7 @@ import (
 
 	ds "github.com/ipfs/go-datastore"
 	"github.com/ipfs/go-datastore/sync"
+	badger "github.com/ipfs/go-ds-badger3"
 	"github.com/libp2p/go-libp2p"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
@@ -157,9 +158,24 @@ func Node1() {
 
 	// ------------------
 
-	// create a new in-memory datastore
-	memoryDatastore := ds.NewMapDatastore()
-	datastore := sync.MutexWrap(memoryDatastore)
+	// // create a new in-memory datastore
+	// memoryDatastore := ds.NewMapDatastore()
+	// datastore := sync.MutexWrap(memoryDatastore)
+
+	// define the path for the persistent datastore
+	path := "./.data-cluster"
+
+	// ensure that the path exists
+	if err := os.MkdirAll(path, os.ModePerm); err != nil {
+		log.Fatalf("Failed to create datastore directory: %v", err)
+	}
+
+	// create a new Badger datastore
+	badgerDatastore, err := badger.NewDatastore(path, &badger.DefaultOptions)
+	datastore := sync.MutexWrap(badgerDatastore)
+	if err != nil {
+		log.Fatalf("Failed to create badger datastore: %v", err)
+	}
 
 	// close datastore when done
 	defer datastore.Close()
